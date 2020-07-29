ceace0e3a79a8b42fc51558ffb6e6c9ff4f7346b
Nathan Pemberton <nathanp@berkeley.edu>
2020-07-29 11:22:29 -0700

    Add memoization to metadata collection in FetchPartRefs. This improves performance on the 16b sort by 3x.

--- benchmark/pkg/data/helpers.go
+++ benchmark/pkg/data/helpers.go
@@ -7,20 +7,33 @@ import (
 )
 
 func FetchPartRefs(refs []*PartRef) ([]byte, error) {
+	var err error
+
 	totalLen := 0
 	for i := 0; i < len(refs); i++ {
 		totalLen += refs[i].NByte
 	}
+
+	// Used to memoize the result of GetParts() which can be expensive
+	arrParts := make(map[DistribArray][]DistribPart)
 
 	// Fetch data to local memory
 	var out = make([]byte, totalLen)
 	inPos := 0
 	for i := 0; i < len(refs); i++ {
 		bktRef := refs[i]
-		parts, err := bktRef.Arr.GetParts()
-		if err != nil {
-			return nil, errors.Wrapf(err, "Couldn't get partitions from input ref %v", i)
+
+		var parts []DistribPart
+		var ok bool
+		if parts, ok = arrParts[bktRef.Arr]; !ok {
+			parts, err = bktRef.Arr.GetParts()
+			if err != nil {
+				return nil, errors.Wrapf(err, "Couldn't get partitions from input ref %v", i)
+			}
+
+			arrParts[bktRef.Arr] = parts
 		}
+
 		reader, err := parts[bktRef.PartIdx].GetRangeReader(bktRef.Start, bktRef.Start+bktRef.NByte)
 		if err != nil {
 			return nil, errors.Wrapf(err, "Couldn't read partition from ref %v", i)
